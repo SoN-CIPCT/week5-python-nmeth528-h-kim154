8ef39fdd92d8ae1a1cb6584def200485813a38f5
h-kim154 <hkim154@uw.edu>
2026-02-07 04:57:58 +0000

    Added homework5_<hyoeun_kim>

---
+++ homework5_<hyoeun_kim>.py
@@ -0,0 +1,20 @@
+fruits = ["strawberry", "blueberry", "banana", "grape", "apple", "peach"]
+print(fruits)
+
+row1 = fruits[0:2]
+print(row1)
+
+row2 = fruits[2:4]
+print(row2)
+
+print(fruits[0], fruits[5])
+
+menu = ("salad", "pasta", "pizza", "sandwich", "cheeseburger")
+print(menu)
+for food in menu:
+    print(food)
+
+menu = ("salad", "pasta", "pizza", "tacos", "burrito")
+print(menu)
+for food in menu:
+    print(food)
